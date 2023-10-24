4b4358a5fed53cbe2feb20f1a9c3472cc3db4a39
Emi Owashi Vallejo <emiowa@hotmail.com>
2023-10-24 17:16:20 +0900

    estilo modificado para el navbar

--- components/Header.js
+++ components/Header.js
@@ -11,30 +11,30 @@ export default function Header(){
 			<div className="container">
 				<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
 					<Link href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-						<span className="fs-6">OAH<br />Architecture</span>
+						<span className="fs-5 text-body lh-sm">OAH<br />Architecture</span>
 					</Link>
 
 
-					<ul className="nav nav-pills">
-						<li className="nav-item"><Link href="/" className={router.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</Link></li>
+					<ul className="nav d-flex align-items-center">
+						<li className="nav-item"><Link href="/" className={router.pathname === '/' ? 'nav-link text-body fw-bold active' : 'nav-link  text-body'}>HOME</Link></li>
 						<div className="dropdown">
-							<Link href="#" className={(router.pathname === '/about/about-me' || router.pathname === '/about/about-oah-architecture') ? 'dropdown-toggle nav-link active' : 'dropdown-toggle nav-link'} data-bs-toggle="dropdown" aria-expanded="false">About</Link>
+							<Link href="#" className={(router.pathname === '/about/about-me' || router.pathname === '/about/about-oah-architecture') ? 'dropdown-toggle nav-link text-body fw-bold active' : 'dropdown-toggle nav-link text-body'} data-bs-toggle="dropdown" aria-expanded="false">ABOUT</Link>
 							<ul className="dropdown-menu text-small shadow">
-								<li><Link className={router.pathname === '/about/about-me' ? 'dropdown-item active' : 'dropdown-item'} href="/about/about-me">About me</Link></li>
-								<li><Link className={router.pathname === '/about/about-oah-architecture' ? 'dropdown-item active' : 'dropdown-item'} href="/about/about-oah-architecture">About OAH Architecture</Link></li>
+								<li><Link className={router.pathname === '/about/about-me' ? 'dropdown-item bg-light fw-bold text-body active' : 'dropdown-item'} href="/about/about-me">About me</Link></li>
+								<li><Link className={router.pathname === '/about/about-oah-architecture' ? 'dropdown-item bg-light fw-bold text-body active' : 'dropdown-item'} href="/about/about-oah-architecture">About OAH Architecture</Link></li>
 							</ul>
 						</div>
 						<div className="dropdown">
-							<Link href="#" className={(router.pathname === '/blog/user-stories' || router.pathname === '/blog/lets-inhabit-architecture' || router.pathname === '/blog/lets-talk-about-architecture') ? 'dropdown-toggle nav-link active' : 'dropdown-toggle nav-link'} data-bs-toggle="dropdown" aria-expanded="false">Blog</Link>
+							<Link href="#" className={(router.pathname === '/blog/user-stories' || router.pathname === '/blog/lets-inhabit-architecture' || router.pathname === '/blog/lets-talk-about-architecture') ? 'dropdown-toggle nav-link text-body fw-bold active' : 'dropdown-toggle nav-link text-body'} data-bs-toggle="dropdown" aria-expanded="false">BLOG</Link>
 							<ul className="dropdown-menu text-small shadow">
-							<li><Link className={router.pathname === '/blog/user-stories' ? 'dropdown-item active' : 'dropdown-item'} href="/blog/user-stories">User stories</Link></li>
-							<li><Link className={router.pathname === '/blog/lets-inhabit-architecture' ? 'dropdown-item active' : 'dropdown-item'} href="/blog/lets-inhabit-architecture">Let's inhabit Architecture</Link></li>
-							<li><Link className={router.pathname === '/blog/lets-talk-about-architecture' ? 'dropdown-item active' : 'dropdown-item'} href="/blog/lets-talk-about-architecture">Let's talk about Architecture</Link></li>
+							<li><Link className={router.pathname === '/blog/user-stories' ? 'dropdown-item bg-light fw-bold text-body active' : 'dropdown-item'} href="/blog/user-stories">User stories</Link></li>
+							<li><Link className={router.pathname === '/blog/lets-inhabit-architecture' ? 'dropdown-item bg-light fw-bold text-body active' : 'dropdown-item'} href="/blog/lets-inhabit-architecture">Let's inhabit Architecture</Link></li>
+							<li><Link className={router.pathname === '/blog/lets-talk-about-architecture' ? 'dropdown-item bg-light fw-bold text-body active' : 'dropdown-item'} href="/blog/lets-talk-about-architecture">Let's talk about Architecture</Link></li>
 							</ul>
 						</div>
-						<li className="nav-item"><Link href="/contact" className={router.pathname === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link></li>
+						<li className="nav-item"><Link href="/contact" className={router.pathname === '/contact' ? 'nav-link text-body fw-bold active' : 'nav-link text-body'}>CONTACT</Link></li>
 						<div className="dropdown">
-							<Link href="#" className="dropdown-toggle nav-link" data-bs-toggle="dropdown" aria-expanded="false">
+							<Link href="#" className="dropdown-toggle nav-link text-body" data-bs-toggle="dropdown" aria-expanded="false">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-globe-americas" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/></svg>
 							</Link>
 							<ul className="dropdown-menu text-small shadow">
@@ -48,12 +48,16 @@ export default function Header(){
 				</header>
 			</div>
 
-			<style jsx>{`
+			{/* <style jsx>{`
 				.active{
+					color: red;
 					font-weight: bold;
 				}
+				.nav{     
+					color: red;
+				}
 				.nav-link{
-					color: #333;
+					color: red;
 					font-family: 'Lato', sans-serif;
 				}
 				span{
@@ -61,7 +65,7 @@ export default function Header(){
 					color: #333;
 					line-height: 1;
 				}
-			`}</style>
+			`}</style> */}
 			<script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossOrigin="anonymous"></script>
 		</div>
     )
